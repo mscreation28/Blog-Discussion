ab79f0d27f6209c070f45c85d1e962642035d568
Shyam Makwana <makwanashyam28@gmail.com>
2020-11-18 23:41:56 +0530

    User Profile

--- Models/User.cs
+++ Models/User.cs
@@ -1,33 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BlogDiscussion2.Models
 {
     public class User: IdentityUser
-    {        
-        [Display(Name = "Name")]
-        public string uName { get; set; }
-
-        [Display(Name = "Email")]
-        [DataType(DataType.EmailAddress)]
-        public string userEmail { get; set; }
-
-        [Display(Name = "Password")]
-        [DataType(DataType.Password)]
-        public string userPassword { get; set; }
-
-        [Display(Name = "Profile Picture")]
-        public string userProfilePicture { get; set; }
-
-        [Display(Name = "Mobile Number")]        
-        public string userMobileNum { get; set; }
-
-        [Display(Name = "Twitter Handle")]
+    {      
         public string userTwitterHandle { get; set; }
 
         [Display(Name = "Instagram Handle")]
@@ -41,10 +26,13 @@ namespace BlogDiscussion2.Models
         [Display(Name = "User Replies")]
         public List<Reply> UserReplies { get; set; }
 
+
         //[Display(Name = "User Liked Blogs")]
         //public List<Blog> UserLikedBlogs { get; set; }
 
         //[Display(Name = "User BookmarkedBlogs")]
-        //public List<Blog> UserBookmarkedBlogs { get; set; }        
+        //public List<Blog> UserBookmarkedBlogs { get; set; }    
+
+
     }
 }
